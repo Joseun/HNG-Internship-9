b0a72752a3c5ff3a67931e974ddf72f9333b5efa
Joseph Ologunja <68515821+Joseun@users.noreply.github.com>
2022-10-31 22:38:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({"slackUsername": "Joseun",
+                    "backend": True,
+                    "age": 27,
+                    "bio": "Hi, Joseph Ologunja here, I am a Full Stack developer, proficient in Python"
+                   })

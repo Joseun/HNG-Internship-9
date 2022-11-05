69020d029169a91c6e76d01d392d0ac32021e0f1
Joseph Ologunja <68515821+Joseun@users.noreply.github.com>
2022-11-06 00:31:11 +0100

    Stage 2

--- app.py
+++ app.py
@@ -11,37 +11,38 @@ def hello_world():
 
 @app.route('/', methods=['POST'])
 def post_world():
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+#     if not request.get_json():
+#         abort(400, description="Not a JSON")
 
-    if 'operation_type' not in request.get_json():
-        abort(400, description="Missing operation_type")
-    if 'x' not in request.get_json():
-        abort(400, description="Missing value")
-    if 'y' not in request.get_json():
-        abort(400, description="Missing value")
-    if not type(request.get_json()['x']) is int:
-        abort(400, description="Value not an integer")
-    if not type(request.get_json()['y']) is int:
-        abort(400, description="Value not an integer")
-    ops = {'addition': x + y, 
-           'substraction': x - y,
-           'multiplication': x * y}
-    data = request.get_json()
-    x = data['x']
-    y = data['y']
-    for k, v in ops.items():
-        if k == data['operation_type']:
-            result = v
+#     if 'operation_type' not in request.get_json():
+#         abort(400, description="Missing operation_type")
+#     if 'x' not in request.get_json():
+#         abort(400, description="Missing value")
+#     if 'y' not in request.get_json():
+#         abort(400, description="Missing value")
+#     if not type(request.get_json()['x']) is int:
+#         abort(400, description="Value not an integer")
+#     if not type(request.get_json()['y']) is int:
+#         abort(400, description="Value not an integer")
+#     ops = {'addition': x + y, 
+#            'substraction': x - y,
+#            'multiplication': x * y}
+#     data = request.get_json()
+#     x = data['x']
+#     y = data['y']
+#     for k, v in ops.items():
+#         if k == data['operation_type']:
+#             result = v
    
     r = {"slackUsername": "Joseun",
-                    "operation_type": data['operation_type'],
-                    "result": result
-                   }
-    headers={'mimetype':'application/json'}
+                     "operation_type": data['operation_type'],
+                     "result": result
+         "data": data
+                    }
+     headers={'mimetype':'application/json'}
 
-    response = make_response(jsonify(r), 200)
+     response = make_response(jsonify(r), 200)
 
-    response.headers = headers
+     response.headers = headers
 
-    return response
+     return response

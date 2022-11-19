2daa0c36e791eaeab4987d12ceee7df57a7e5a05
Joseph Ologunja <68515821+Joseun@users.noreply.github.com>
2022-11-19 07:25:12 +0000

    fastapi branch

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-# README
+# Deploy FastAPI on Render
 
-This is the HNG Internship repository using [Flask](http://flask.pocoo.org/) and [Python](https://www.python.org/) 
+Use this repo as a template to deploy a Python [FastAPI](https://fastapi.tiangolo.com) service on Render.
 
- Flask [quick start](http://flask.pocoo.org/docs/1.0/quickstart/#a-minimal-application) example for [Render](https://render.com).
+## Steps
 
-The app in this repo is deployed at [https://flask.onrender.com](https://flask.onrender.com).
+1. You may use this repository directly or [create your own repository from this template](https://github.com/render-examples/fastapi/generate) if you'd like to customize the code.
+2. Create a new Web Service on Render.
+3. Specify the URL to your new repository or this repository.
+4. Render will automatically detect that you are deploying a Python service and use `pip` to download the dependencies.
+5. Specify the following as the Start Command.
 
-## Deployment
+    ```shell
+    uvicorn main:app --host 0.0.0.0 --port 10000
+    ```
 
-Follow the guide at https://render.com/docs/deploy-flask.
+6. Click Create Web Service.
+
+See https://render.com/docs/deploy-fastapi for more details.
+
+## Thanks
+
+Thanks to [Harish](https://harishgarg.com) for the [inspiration to create a FastAPI quickstart for Render](https://twitter.com/harishkgarg/status/1435084018677010434) and for some sample code!
--- app.py
+++ app.py
@@ -1,49 +1,20 @@
-from flask import Flask, jsonify, make_response, request
-app = Flask(__name__)
+#!/usr/bin/python3
+""" POST ENDPOINT FOR ACCOUNT STATEMENT """
+from fastapi import FastAPI, File, UploadFile
+# from models import Account_statement
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return jsonify({"slackUsername": "Joseun",
-                    "backend": True,
-                    "age": 27,
-                    "bio": "Hi, Joseph Ologunja here, I am a Full Stack developer, proficient in Python"
-                   })
+app = FastAPI()
 
-@app.route('/', methods=['POST'])
-def post_world():
-#     if not request.get_json():
-#         abort(400, description="Not a JSON")
 
-#     if 'operation_type' not in request.get_json():
-#         abort(400, description="Missing operation_type")
-#     if 'x' not in request.get_json():
-#         abort(400, description="Missing value")
-#     if 'y' not in request.get_json():
-#         abort(400, description="Missing value")
-#     if not type(request.get_json()['x']) is int:
-#         abort(400, description="Value not an integer")
-#     if not type(request.get_json()['y']) is int:
-#         abort(400, description="Value not an integer")
-     data = request.get_json()
-     x = data['x']
-     y = data['y']
-     result = 0
-     op = data['operation_type']
-     if op == 'addition':
-        result = x + y
-     if op == 'substraction':
-        result = x - y
-     if op == 'multiplication':
-        result = x * y
-        
-     r = {"slackUsername": "Joseun",
-           "operation_type": op,
-           "result": result}
-
-     headers={'Content-Type':'application/json'}
-
-     response = make_response(jsonify(r), 200)
-
-     response.headers = headers
-
-     return response
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    """ Uploads file to database """
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+    return {"message": f"Successfully uploaded {file.filename}"}

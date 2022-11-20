872cee0407dfd33ce699fd07f414bbd73021f9bf
Joseph Ologunja <68515821+Joseun@users.noreply.github.com>
2022-11-20 17:53:32 +0100

    Updated

--- app.py
+++ app.py
@@ -1,53 +1,35 @@
+
+    
 #!/usr/bin/python3
-# """ POST ENDPOINT FOR ACCOUNT STATEMENT """
-# from fastapi import FastAPI, File, UploadFile
-# # from models import Account_statement
 
-# app = FastAPI()
+""" POST ENDPOINT FOR ACCOUNT STATEMENT """
 
+from fastapi import FastAPI, File, UploadFile
 
-# @app.post("/upload")
-# def upload(file: UploadFile = File(...)):
-#     """ Uploads file to database """
-#     try:
-#         contents = file.file.read()
-#         with open(file.filename, 'wb') as f:
-#             f.write(contents)
-#     except Exception:
-#         return {"message": "There was an error uploading the file"}
-#     finally:
-#         file.file.close()
-#     return {"message": f"Successfully uploaded {file.filename}"}
-from typing import Union
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+# from models import Account_statement
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+@app.post("/upload")
 
-@app.get("/", status_code=200)
-def html_response():
-    html_content = """ 
-        <html>
-        <head>
-            <title>Account Pal</title>
-        </head>
-        <body>
-            <h1>Welcome to Account Pal 🙂</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+def upload(file: UploadFile = File(...)):
 
+    """ Uploads file to database """
 
-@app.get("/", status_code=200, response_class=HTMLResponse)
-def landing_page():
-    return html_response()
+    try:
+
+        contents = file.file.read()
+
+        with open(file.filename, 'wb') as f:
+
+            f.write(contents)
+
+    except Exception:
+
+        return {"message": "There was an error uploading the file"}
+
+    finally:
+
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
